a8bbd546243d6bf8d0cab530ec419611291a9d61
owais9061 <owaischaudhry906@gmail.com>
2023-03-24 17:19:34 +0500

    Added 2 more programs

--- Learning-Java/BasicJava/src/Basics/payRoll.java
+++ Learning-Java/BasicJava/src/Basics/payRoll.java
@@ -1,6 +1,6 @@
 package Basics;
 
-public class simple01{
+public class payRoll{
     public static void main(String[]args){
         int hours = 40;
         double grossPay, payRate=25.0;
---
+++ Learning-Java/BasicJava/src/Basics/studentInfo.java
@@ -0,0 +1,21 @@
+package Basics;
+
+public class studentInfo {
+    public static void main(String[]args) {
+        int studentAge = 19;
+        char firstInitial = 'O';
+        char lastInitial = 'G';
+        double gpa = 3.3;
+        boolean perfectAttendace= true;
+        String name= "Owais Gujjar";
+    
+        System.out.println("STUDENT INFORMATION CENTER");
+        System.out.println("Name: " + name);
+        System.out.println("Age is: " + studentAge);
+        System.out.println("First Initial is: " + firstInitial);
+        System.out.println("Last Initial is: " + lastInitial);
+        System.out.println("GPA is: " + gpa);
+        System.out.println("Perfectly Present: " + perfectAttendace);
+       
+    }
+}
