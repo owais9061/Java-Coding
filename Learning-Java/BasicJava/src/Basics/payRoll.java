f052543527fe5a6c205439c441b4fc18e8a5dde0
owais9061 <owaischaudhry906@gmail.com>
2023-03-24 17:07:11 +0500

    Done with simple payroll program

---
+++ Learning-Java/BasicJava/src/Basics/payRoll.java
@@ -0,0 +1,10 @@
+package Basics;
+
+public class simple01{
+    public static void main(String[]args){
+        int hours = 40;
+        double grossPay, payRate=25.0;
+        grossPay= hours*payRate;
+        System.out.println("Your pay is:  $" + grossPay);
+    }
+}
