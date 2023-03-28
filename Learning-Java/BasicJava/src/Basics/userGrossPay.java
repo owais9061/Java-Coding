687adee667c660a45d1e5900ccd56313e7a425ec
owais9061 <owaischaudhry906@gmail.com>
2023-03-28 06:50:43 +0500

    write 2 more programs userGrossPay & salesTax Program

---
+++ Learning-Java/BasicJava/src/Basics/userGrossPay.java
@@ -0,0 +1,34 @@
+//!This program calculates the user's gross pay
+package Basics;
+
+import java.util.Scanner; //to be able to read from the keyboard
+
+public class userGrossPay
+{
+ public static void main(String [] args)
+ {
+
+ //?create a Scanner object to read from the keyboard
+ Scanner input = new Scanner(System.in);
+
+ //?identifier declarations
+ double hours; //number of hours worked
+ double rate; //hourly pay rate
+ double pay; //gross pay
+ 
+ //?display prompts and get input
+ System.out.print("How many hours did you work? ");
+ hours = input.nextDouble();
+ System.out.print("How much do you get paid per hour? ");
+ rate = input.nextDouble();
+ 
+ //?calculations
+ if(hours <= 40)
+ pay = hours * rate;
+ else
+ pay = (hours - 40) * (1.5 * rate) + 40 * rate;
+ 
+ //?display results
+ System.out.println("You earned $" + pay);
+ }
+}
