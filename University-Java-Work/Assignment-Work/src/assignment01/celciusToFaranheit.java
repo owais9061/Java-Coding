b326b0bc29ab35d59433c9ddac70b47c7b737e75
owais9061 <owaischaudhry906@gmail.com>
2023-04-03 12:28:31 +0500

    Done questio#01

---
+++ University-Java-Work/Assignment-Work/src/assignment01/celciusToFaranheit.java
@@ -0,0 +1,21 @@
+/**Conversion between Celsius and Fahrenheit Write a class that contain the following two method:
+Public static double Celsius  to Fahrenheit (double Celsius) 
+Public static double Fahrenheit to Celsius  to (double Fahrenheit) 
+The formula for the conversion is:
+Fahrenheit=(9.0/5)*celsius+32
+Celsius=(5.0/9)*( Fahrenheit-32) **/
+
+package assignment01;
+import java.util.Scanner;
+
+public class celciusToFaranheit{
+    public static void main (String[]args){
+        double celcius, faranheit;
+        System.out.println("SIMPLE CELCIUS TO FARANHEIT CALCULATOR");
+        Scanner input = new Scanner(System.in);
+        System.out.println("Input Temperature in Ceclcius: ");
+        celcius = input.nextInt();
+        faranheit = (9.0/5)* celcius +32;
+        System.out.println(celcius + "* Celcius Temperature in Faranheit is: " + faranheit);
+    }
+}
