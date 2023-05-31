b8e1b8bb850453f19ae3a86a4d8f919bfc0be6d0
owais9061 <owaischaudhry906@gmail.com>
2023-05-31 17:38:19 +0500

    Done with experiment#09

---
+++ University-Java-Work/Lab-Work/src/experiment09/Main.java
@@ -0,0 +1,9 @@
+package experiment09;
+// Main class for testing
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.crawl();
+        animal.move();
+    }
+}
